0c09c7e34c36445b77ab2aa1e600d1ea7784f296
vigransamerpitak <vigran.samerpitak@gmail.com>
2026-06-16 17:18:52 +0700

    V.16062026005

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js - ไฟล์เก็บค่าเชื่อมต่อฐานข้อมูล Supabase
-const SUPABASE_URL = "https://tseftsnzyrbcrajearxz.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://tseftsnzyrbcrajearxz.supabase.co";
 const SUPABASE_KEY = "sb_publishable_0cfkLjDHdpoDicbNNf68OA_ZWsZdPak";
 
 // เริ่มต้นเชื่อมต่อ Supabase Client
